5a272a78096189453e7cae541139debe3cae4f40
AJ <ajdibo23@gmail.com>
2023-11-16 23:19:58 -0600

    improved formatting on search sub counts

--- js/fix.js
+++ js/fix.js
@@ -3,6 +3,7 @@ function fix(input) {
   if (input.includes('Mio.')) {
     input = input.replace('Â Mio.', 'M');
     input = input.replace(',', '.');
+    input = input.replace('Mio.', 'M');
     if (input.includes('000')) {
       input = input.replace('.000', 'K');
     } else if (input.includes('00')) {
@@ -13,7 +14,12 @@ function fix(input) {
       input = input.replace(',', '.');
       input = input.replace(' Subscribers', 'K Subscribers');
     }
+    if (input.split(' Subscribers')[0].length == 4) {
+      input = (input.split(' Subscribers')[0])
+      input = parseInt(input).toLocaleString('en-US') + ' Subscribers';
+    }
   }
+  input = input.replace('.000', 'K');
   if (input == '0') {
     input = '0 Subscribers';
   }
