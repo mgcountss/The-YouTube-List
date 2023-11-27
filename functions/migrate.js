52baccb012261e0e37b5ebc488f0aeb1b8245d65
AJ <ajdibo23@gmail.com>
2023-11-27 11:12:08 -0600

    migrate

---
+++ functions/migrate.js
@@ -0,0 +1,23 @@
+import newDB from './db.js';
+import oldDB from './old-db.js';
+import fs from 'fs';
+let load = await oldDB.initalLoad();
+console.log(load);
+let soFar = 0;
+
+let oldChannels = await oldDB.getall({
+    sort1: "subscribers",
+    sort2: "name",
+    order1: 'desc',
+    order2: 'desc',
+    limit: 1000000,
+    offset: 0,
+    search: '',
+    filters: []
+});
+fs.writeFileSync("./old.json", JSON.stringify(oldChannels, {}));
+for (let channel of oldChannels.documents) {
+    soFar++;
+    newDB.add(channel);
+    console.log(`Added ${soFar} of ${oldChannels.total}`);
+}
